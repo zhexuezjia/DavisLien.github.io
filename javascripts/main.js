ac09bc892da9340d9f6f63ef95d1542a0ab46431
davislien@163.com <davislien@163.com>
2015-09-01 20:50:56 +0800

    不让别人用iframe来打开自己的博客

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,32 @@
-console.log('This would be the main JS file.');
+$(function() {
+
+  var one = {
+    init: function() {
+      this.baseEvents();
+    },
+    baseEvents: function() {
+      if (window.location != window.parent.location) window.parent.location = window.location;
+    }
+  }
+
+  var two = {
+    init: function() {
+      this.baseEvents();
+    },
+    baseEvents: function() {
+
+    }
+  }
+
+  var main = {
+    init: function() {
+      this.baseEvents();
+    },
+    baseEvents: function() {
+      one.init();
+      two.init();
+    }
+  }
+  main.init();
+
+});
